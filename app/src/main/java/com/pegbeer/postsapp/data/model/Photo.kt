330030e9e9c42200a27f3ec0bff798d9c99bc870
Pegbeer <55509957+Pegbeer@users.noreply.github.com>
2023-02-04 21:31:58 -0600

    Models added ✔️

---
+++ app/src/main/java/com/pegbeer/postsapp/data/model/Photo.kt
@@ -0,0 +1,9 @@
+package com.pegbeer.postsapp.data.model
+
+data class Photo(
+    val albumId:Int,
+    val id:Int,
+    val title:String,
+    val url:String,
+    val thumbnailUrl:String
+)
---
+++ app/src/main/java/com/pegbeer/postsapp/data/model/Post.kt
@@ -0,0 +1,8 @@
+package com.pegbeer.postsapp.data.model
+
+data class Post(
+    val userId:Int,
+    val id:Int,
+    val title:String,
+    val body:String
+)
